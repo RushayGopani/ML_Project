f2adb4806c29bbf5532a4908287b35c1ea232e2e
RushayGopani <rushayg5521@gmail.com>
2026-06-22 17:42:47 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirement(file_path: str)->List[str]:
+    '''
+    this function will return the list of requiremnet
+    '''
+    requirement = []
+    with open(file_path) as file_obj : 
+        requirement = file_obj.readlines()
+        requirement = [req.strip() for req in requirement]
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+    
+    return requirement
+    
+setup(
+    name = 'ML_Project',
+    version='0.0.1',
+    author = 'Rushay',
+    author_email='rushayg5521@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirement('requirement.txt')
+    
+    
+)
